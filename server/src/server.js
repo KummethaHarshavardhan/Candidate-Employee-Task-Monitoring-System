65840c68d9b0371045ee7f809f584f14c6f048cf
Harshavardhan <2theharshavardhankummetha@gmail.com>
2026-08-28 16:10:43 +0530

    modified server file

--- server/src/server.js
+++ server/src/server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
+
 const candidateRoutes = require('./routes/candidateRoutes');
+const taskRoutes = require('./routes/taskRoutes');
+const assignmentRoutes = require('./routes/assignmentRoutes');
 
 dotenv.config();
 
@@ -16,6 +19,8 @@ app.use(cors({
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api/candidates', candidateRoutes);
+app.use('/api/tasks', taskRoutes);
+app.use('/api/assignments', assignmentRoutes);
 const PORT = process.env.PORT || 5000;
 
 const server = app.listen(PORT, () => {
